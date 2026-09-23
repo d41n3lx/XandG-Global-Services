5b1f87693ba088bcee67be6a701d4b4037d5e922
Daniel G <dapereira519axa@gmail.com>
2026-09-23 04:07:27 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+    //Script//
+    tailwind.config = {
+            theme: {
+                extend: {
+                    colors: {
+                        navy: {
+                            DEFAULT: '#071931',
+                            dark: '#031021',
+                            light: '#0c2445',
+                            card: '#0e294d'
+                        },
+                        gold: {
+                            DEFAULT: '#E3B341',
+                            hover: '#cda032',
+                            light: '#f5d688',
+                            dark: '#a87f1f'
+                        }
+                    },
+                    fontFamily: {
+                        sans: ['Plus Jakarta Sans', 'sans-serif'],
+                        motto: ['Great Vibes', 'cursive']
+                    }
+                }
+            }
+        }
